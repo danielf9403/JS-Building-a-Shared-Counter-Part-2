94f1b94782124f7c1f5f5508c9d9a2b93a109595
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2023-02-21 23:01:42 -0500

    fetch

--- public/index.js
+++ public/index.js
@@ -2,13 +2,13 @@ async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
-
+    const confirmButton = document.querySelector('#confirm-button');
+    const inputField = document.querySelector('#user-init-value-setup');
     const response = await fetch('http://localhost:9001/counter');
-
     const result = await response.json();
     
     let countValue = result.value;
-
+    let userImputValue = 0;
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
@@ -19,8 +19,26 @@ async function main(){
         countContainer.textContent = countValue;
     }
 
+    function handeleImput(e) {
+        userImputValue = e.target.value;
+    }
+
+    async function handleConfirmation() {
+        await fetch('http://localhost:9001/counter', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                value: Number(userImputValue),
+            }),
+        });
+        main();
+    }
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    inputField.addEventListener("change");
+    confirmButton.addEventListener("click", confirm);
     countContainer.textContent = countValue;
 }
 main()
